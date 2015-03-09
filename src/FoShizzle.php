834c6fc6cf705a54e4355de72b8ae465ca59b7cf
Kyle Bulloch & Kyle Giard-Chase <student@epicodus.com>
2015-03-09 16:26:57 -0700

    Add test for capital letter (remove two early tests)

--- src/FoShizzle.php
+++ src/FoShizzle.php
@@ -8,12 +8,13 @@
 
             $i=0;
             foreach ($input_letters as $letter) {
-                if ($letter == "s") {
+                if ($letter == "s" && $i != 0 ) {
                     array_push($output_letters, "z");
                 }
                 else {
                     array_push($output_letters, $letter);
                 }
+                ++$i;
             }
 
             return implode($output_letters);
--- tests/FoShizzleTest.php
+++ tests/FoShizzleTest.php
@@ -4,32 +4,6 @@
 
     class FoShizzleTest extends PHPUnit_Framework_TestCase
     {
-        function testFoShizzleOneLetter()
-        {
-            //Arrange
-            $test_fo_shizzle_one_letter = new FoShizzle;
-            $input = "s";
-
-            //Act
-            $result = $test_fo_shizzle_one_letter->changeLetters($input);
-
-            //Assert
-            $this->assertEquals("z", $result);
-        }
-
-        function testFoShizzleMultiLetters()
-        {
-            //Arrange
-            $test_fo_shizzle_multi_letters = new FoShizzle;
-            $input = 'ss';
-
-            //Act
-            $result = $test_fo_shizzle_multi_letters->changeLetters($input);
-
-            //Assert
-            $this->assertEquals('zz', $result);
-        }
-
         function testFoShizzleOneWord()
         {
             //Arrange
@@ -55,6 +29,19 @@
 
             //Assert
             $this->assertEquals("start", $result);
+        }
+
+        function testFoShizzleCapitalS()
+        {
+            //Arrange
+            $test_fo_shizzle_capital_s = new FoShizzle;
+            $input = "iS";
+
+            //Act
+            $result = $test_fo_shizzle_capital_s->changeLetters($input);
+
+            //Assert
+            $this->assertEquals("iS", $result);
         }
     }
 ?>
