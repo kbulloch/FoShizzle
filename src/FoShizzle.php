4eccd1cf4bb560092a8725057e1720ba53d1b078
Kyle Bulloch & Kyle Giard-Chase <student@epicodus.com>
2015-03-09 16:08:00 -0700

    Add multi letter test and function (and word test)

--- src/FoShizzle.php
+++ src/FoShizzle.php
@@ -1,9 +1,25 @@
 <?php
     class FoShizzle
     {
-        function changeLetters()
+        function changeLetters($input)
         {
-            
+            // if( $input == 's' ) {
+            //     return 'z';
+            // }
+
+            // $exploded_input = explode("", $input);
+            $input_letters = str_split($input);
+            $output_letters = array();
+
+            $i=0;
+            foreach ($input_letters as $letter) {
+                if ($letter == "s") {
+                    array_push($output_letters, "z");
+                }
+            }
+
+            return implode($output_letters);
+
         }
     }
 ?>
--- tests/FoShizzleTest.php
+++ tests/FoShizzleTest.php
@@ -16,5 +16,32 @@
             //Assert
             $this->assertEquals("z", $result);
         }
+
+        function testFoShizzleMultiLetters()
+        {
+            //Arrange
+            $test_fo_shizzle_multi_letters = new FoShizzle;
+            $input = 'ss';
+
+            //Act
+            $result = $test_fo_shizzle_multi_letters->changeLetters($input);
+
+            //Assert
+            $this->assertEquals('zz', $result);
+        }
+
+        function testFoShizzleOneWord()
+        {
+            //Arrange
+            $test_fo_shizzle_one_word = new FoShizzle;
+            $input = "bass";
+
+            //Act
+            $result = $test_fo_shizzle_one_word->changeLetters($input);
+
+            //Assert
+            $this->assertEquals("bazz", $result);
+
+        }
     }
 ?>
